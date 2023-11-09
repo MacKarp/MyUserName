f45232bf8b6a8a0ec9b0fc1f400ffd017f161469
Karpfly <karpfly@gmail.com>
2023-11-09 09:09:29 +0100

    update to get username from AzureAD login

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,21 @@
 use chrono::prelude::*;
 use copypasta::{ClipboardContext, ClipboardProvider};
+use std::process::Command;
 
 fn main() {
-    let username = whoami::username();
+    let cmd = Command::new("cmd")
+        .args(["/C", "whoami.exe /UPN"])
+        .output()
+        .expect("failed to execute process")
+        .stdout;
+    let full_user_name = String::from_utf8(cmd).unwrap();
+
+    let username: Vec<&str> = full_user_name.split('@').collect();
+    let username = *username.first().unwrap();
+
     let current_time = Local::now().format("%d-%m-%Y %H:%M").to_string();
     let message = format!("{current_time} {username} \n");
 
     let mut clipboard_ctx = ClipboardContext::new().unwrap();
     clipboard_ctx.set_contents(message).unwrap();
-
-    // without getting from clip it is not working on linux - tested on Gnome Fedora 36
-    clipboard_ctx.get_contents().unwrap();
 }
